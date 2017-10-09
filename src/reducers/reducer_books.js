6f70b0328ec18019594a20e554e9a314b4e12268
agnieszkabugla <agnieszka.bugla3@gmail.com>
2017-10-09 14:37:27 +0200

    last commit - simple react-redux exercies, selecting books from BookList

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        { title: 'Javascript: the good parts', pages: 302 },
+        { title: 'Harry Potter', pages: 453 }, 
+        { title: 'The Dark Tower', pages: 187 }, 
+        { title: 'Eloquent Ruby', pages: 3 },
+        { title: 'Mastering jQuery', pages: 203}        
+    ]
+}; 
